bf9aedc486446fb8c13c62fc1016fb2fce4a256d
Ronny <pettersen.ronny@gmail.com>
2017-06-05 09:49:48 +0200

    Update index.php
    Added hostname

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-echo <<<EOT
+?>
 <HTML>
 <HEAD><Title>Index Page</title></head>
 <BODY>
 <h1>Hello Chandigarh!</h1>
-</BODY></HTML>
-EOT;
+<?php
+  echo "You are on ". gethostname() . "\n";
 ?>
+</BODY></HTML>
