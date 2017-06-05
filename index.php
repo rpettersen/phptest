777dc76f930c52910870bef0e45daa0ff14a50c2
Ronny <pettersen.ronny@gmail.com>
2017-06-05 08:59:17 +0200

    Update index.php
    Changed to heredoc syntax

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-print "<HTML>\n";
-print "<HEAD><Title>Index Page</title></head>\n";
-print "<BODY>\n";
-print "<h1>Hello Chandigarh!</h1>\n";
-print "</BODY></HTML>\n";
+echo <<<EOT
+<HTML>
+<HEAD><Title>Index Page</title></head>
+<BODY>
+<h1>Hello Chandigarh!</h1>
+</BODY></HTML>
+EOT;
 ?>
